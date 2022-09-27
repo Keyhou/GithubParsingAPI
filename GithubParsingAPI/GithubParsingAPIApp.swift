4003bcfb506327de3e651ba1d5c0960c367eaa15
Keyhou <31210145+Keyhou@users.noreply.github.com>
2022-09-27 11:04:43 +0200

    Initial Commit

---
+++ GithubParsingAPI/GithubParsingAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubParsingAPIApp.swift
+//  GithubParsingAPI
+//
+//  Created by Keyhan Mortezaeifar on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GithubParsingAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
